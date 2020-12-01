26fd34be7ebecf6eedab129aa35f29c84302d3a8
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2020-12-01 08:37:14 +0100

    add day 1 part 1 & 2

--- day01/src/lib.rs
+++ day01/src/lib.rs
@@ -1,6 +1,30 @@
 
-pub fn count_l(input: &str) -> i32 {
-    input.chars().filter(|c| *c == 'l').count() as i32
+pub fn parse(input: &str) -> Vec<i32> {
+    input.lines().map(|line| line.parse::<i32>().unwrap()).collect()
+}
+
+pub fn part1(input: &[i32]) -> i32 {
+    for first in input {
+        for second in input {
+            if first + second == 2020 {
+                return first * second
+            }
+        }
+    }
+    unreachable!()
+}
+
+pub fn part2(input: &[i32]) -> i32 {
+    for first in input {
+        for second in input {
+            for third in input {
+                if first + second + third == 2020 {
+                    return first * second * third
+                }
+            }
+        }
+    }
+    unreachable!()
 }
 
 #[cfg(test)]
@@ -9,7 +33,8 @@ mod tests {
     #[test]
     fn run() {
         let input = include_str!("./input.txt");
-        println!("{}", input);
-        assert_eq!(count_l(input), 3)
+        let parsed = parse(input);
+        println!("{:?}", part1(&parsed));
+        println!("{:?}", part2(&parsed));
     }
 }
