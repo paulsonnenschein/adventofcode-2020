147d65f9c84a1d4d5946ea22ed76bbf75478ebc7
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2020-12-02 10:56:54 +0100

    add day 2 part 2

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-type Password = (u32, u32, char, String);
+type Password = (usize, usize, char, String);
 
 pub fn parse(input: &str) -> Vec<Password> {
     let pattern = Regex::new(r"^(\d+)-(\d+) (\w): (\w+)$").unwrap();
@@ -19,14 +19,25 @@ pub fn parse(input: &str) -> Vec<Password> {
         .collect()
 }
 
-pub fn part1(input: &[Password]) -> u32 {
-    input.iter().filter(|&pw| is_valid(pw)).count() as u32
+pub fn part1(input: &[Password]) -> usize {
+    input.iter().filter(|&pw| is_valid(pw)).count()
 }
 
 fn is_valid(pw: &Password) -> bool {
     let count = pw.3.chars().filter(|&c| c == pw.2).count();
 
-    ((pw.0)..=(pw.1)).contains(&(count as u32))
+    ((pw.0)..=(pw.1)).contains(&count)
+}
+
+pub fn part2(input: &[Password]) -> usize {
+    input.iter().filter(|&pw| is_valid2(pw)).count()
+}
+
+fn is_valid2(pw: &Password) -> bool {
+    let f = pw.3.chars().nth(pw.0 - 1).map_or(false, |c| c == pw.2);
+    let s = pw.3.chars().nth(pw.1 - 1).map_or(false, |c| c == pw.2);
+
+    f && !s || !f && s
 }
 
 #[cfg(test)]
@@ -38,6 +49,6 @@ mod tests {
         let parsed = parse(input);
         //println!("{:?}", parsed);
         println!("{:?}", part1(&parsed));
-        //println!("{:?}", part2(&parsed));
+        println!("{:?}", part2(&parsed));
     }
 }
