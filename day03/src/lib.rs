0df2eb94da066e71d268ea56b4ece6e21c4a96b8
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2020-12-03 09:00:59 +0100

    add day 3 part 1 & 2

---
+++ day03/src/lib.rs
@@ -0,0 +1,45 @@
+pub fn parse(input: &str) -> Vec<&str> {
+    input.lines().collect()
+}
+
+pub fn part1(input: &[&str]) -> u64 {
+    calculate(input, 3, 1)
+}
+
+pub fn part2(input: &[&str]) -> u64 {
+    calculate(input, 1, 1)
+        * calculate(input, 3, 1)
+        * calculate(input, 5, 1)
+        * calculate(input, 7, 1)
+        * calculate(input, 1, 2)
+}
+
+fn calculate(input: &[&str], right: usize, down: usize) -> u64 {
+    input
+        .iter()
+        .enumerate()
+        .filter(|(i, &_str)| i % down == 0)
+        .enumerate()
+        .map(|(i, (_, &str))| {
+            if str.chars().cycle().nth(i * right).unwrap_or(' ') == '#' {
+                1
+            } else {
+                0
+            }
+        })
+        .sum::<u64>()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run03() {
+        let input = include_str!("./input.txt");
+        let parsed = parse(input);
+        //println!("{:?}", parsed);
+        println!("{:?}", part1(&parsed));
+        println!("{:?}", part2(&parsed));
+    }
+}
